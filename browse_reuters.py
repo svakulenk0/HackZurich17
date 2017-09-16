51a89c2d9b63dfbca4f0641d1cad2fadc5654fe5
vendi <svitlana.vakulenko@gmail.com>
2017-09-16 09:44:19 +0200

    new: annotation aggregator on the TR API

--- browse_reuters.py
+++ browse_reuters.py
@@ -9,7 +9,7 @@ skurikhin, vakulenko
 import urllib2
 import urllib
 import time
-import json
+from collections import Counter
 
 from xml.etree.ElementTree import ElementTree, fromstring
 
@@ -59,13 +59,13 @@ def fetch_channels():
     print ("\n".join(["\t%(alias)s\t%(description)s"%x for x in channels]))
 
 
-def fetch_channel(channel_id='AdG977'):
+def fetch_channel(channel_id='Wbz248', n=2):
     # fetch id's and headlines for a channel
     rd = ReutersDatasource()
     tree = rd.call('items',
                    {'channel': channel_id,
                     'channelCategory':'OLR',
-                    'limit':'10'})
+                    'limit': str(n)})
     items = [ {'id':c.findtext('id'),
                'headline':c.findtext('headline')}
               for c in tree.findall('result') ]
@@ -92,17 +92,44 @@ def get_entity_attrs(entity):
     return themes, theme_names
 
 
+def fetch_annotations(article_id):
+    categories = []
+    article_entities = []
+
+    rd = ReutersDatasource()
+    item = rd.call('itemEntities',{'id': article_id,})
+    
+    tags = item.find('item').findall('tag')
+    
+    if tags:
+        for tag in tags:
+            categories.append(tag.find('name').text)
+
+    entities = item.find('item').findall('entity')
+    
+    if entities:
+        for entity in entities:
+            attribute = entity.find('attribute')
+            if attribute is not None:
+                attribute_text = attribute.find('value').text
+                article_entities.append(attribute_text)
+
+    return categories, article_entities
+
+
 def fetch_item_tags(article_id):
     rd = ReutersDatasource()
     item = rd.call('itemEntities',{'id': article_id,})
     tags = item.find('item').findall('tag')
     if tags:
+        categories = []
         for tag in tags:
-            print tag.find('name').text
+            categories.append(tag.find('name').text)
+        return categories
 
 
 def test_fetch_item_tags():
-    fetch_item_tags('tag:reuters.com,2017:newsml_L2N1LW2CE:708688512')
+    print fetch_item_tags('tag:reuters.com,2017:newsml_L2N1LW2CE:708688512')
 
 
 def fetch_item_entity(article_id):
@@ -131,26 +158,39 @@ def test_fetch_item_entities():
 
 
 def lookup_tags():
-    items = fetch_channel()
-    for item in items:
-        entities = fetch_item_tags(item['id'])
-        if entities:
-            for entity in entities:
-                attributes = entity.findall('attribute')
-                
-                themes = []
-                theme_names = []
-                for attribute in attributes:
-                    value = attribute.find('value')
-                    name = attribute.find('name')
-                    themes.append(value.text)
-                    theme_names.append(name.text)
-                    
-                print themes, theme_names
+    headlines = set()
+    tags = []
+    entities = []
+    articles = fetch_channel(n=100)
+    for article in articles:
+        article_title = article['headline']
+        if article_title not in headlines:
+            headlines.add(article_title)
+            article_id = article['id']
+            # print article_id, article_title
+            article_tags, article_entities = fetch_annotations(article_id)
+            if article_tags:
+                tags.extend(article_tags)
+            if article_entities:
+                entities.extend(article_entities)
+
+    # aggregate annotations summary
+    tags = Counter(tags)
+    entities = Counter(entities)
+
+    print tags
+    print entities
+    #     tags = article.find('item').findall('tag')
+    #     if tags:
+    #         categories = []
+    #         for tag in tags:
+    #             categories.append(tag.find('name').text)
+    #         print categories
 
 
 if __name__=='__main__':
     # lookup_tags()
     # fetch_channels()
     # test_fetch_channel()
-    test_fetch_item_tags()
+    # test_fetch_item_tags()
+    lookup_tags()
