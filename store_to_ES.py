d384a838b91718538a4620b0029a4c702739bc85
vendi <svitlana.vakulenko@gmail.com>
2017-09-16 15:18:55 +0200

    new: tags and entities aggregator to utterances

--- store_to_ES.py
+++ store_to_ES.py
@@ -9,7 +9,7 @@ from elasticsearch import Elasticsearch
 
 from browse_reuters_json import *
 
-index_name='thomson'
+TR_INDEX = 'thomson'
 
 mapping = {
       'settings': {
@@ -22,6 +22,7 @@ mapping = {
           'properties': {
             'headline': {'type': 'text', 'analyzer': 'english'},
             'annotations.entities.name': {'type': 'keyword'},
+            'annotations.tags.name': {'type': 'keyword'},
           }
         },
       }
@@ -33,8 +34,8 @@ def load_articles_in_ES():
 
     # reset index
     try:
-        es.indices.delete(index=index_name)
-        es.indices.create(index=index_name, body=mapping)
+        es.indices.delete(index=TR_INDEX)
+        es.indices.create(index=TR_INDEX, body=mapping)
     except Exception as e:
         print (e)
 
@@ -46,35 +47,83 @@ def load_articles_in_ES():
         annotations = fetch_annotations(article['id'])
         # print annotations
         doc['annotations'] = annotations
-        es.index(index=index_name, doc_type='articles',
+        es.index(index=TR_INDEX, doc_type='articles',
                  body=doc)
 
 
 def check_n_docs():
     es = Elasticsearch()
-    res = es.search(index=index_name, body={"query": {"match_all": {}}})
+    res = es.search(index=TR_INDEX, body={"query": {"match_all": {}}})
     print("Total: %d articles" % res['hits']['total'])
 
 
 def show_one():
     es = Elasticsearch()
-    result = es.search(index=index_name, body={"query": {"match_all": {}}})
-    print(result['hits']['hits'][0])
+    result = es.search(index=TR_INDEX, body={"query": {"match_all": {}}})['hits']['hits'][0]
+    print json.dumps(result, indent=4, sort_keys=True)
+
+class ESClient():
+
+    def __init__(self, index):
+        self.es = Elasticsearch()
+        self.index = index
+
+    def aggregate(self):
+        '''
+        returns the most popular keywords
+        '''
+        result = self.es.search(index=self.index, body={"query": {"match_all": {}}, "aggs": {"tags": {"terms": {"field": "annotations.tags.name"}}, "entities": {"terms": {"field": "annotations.entities.name"}}}})
+        # return json.dumps(result, indent=4, sort_keys=True)
+        return result['aggregations']
+        # return result['aggregations']['popular_terms']['buckets']
+
+    def explore_trend(self, keyword):
+        result = self.es.search(index=self.index, body={"query": {"match": {"annotations.entities.name": keyword}}, "aggs": {"popular_entities": {"terms": {"field": "annotations.entities.name"}}}})
+        return result['aggregations']['popular_terms']['buckets']
+        # print result['hits']['hits']
+        # articles = [article['_id'] for article in result['hits']['hits']]
+        # print len(articles)
+
+    def find_sample_article_by_keyword(self, keyword):
+        result = self.es.search(index=self.index, body={"query": {"match": {"annotations.entities.name": keyword}}})
+        print(result['hits']['hits'][0]['_source']['headline'])
 
 
-def aggregate():
-    '''
-    returns the most popular keywords
-    '''
-    es = Elasticsearch()
-    result = es.search(index=index_name, body={"query": {"match_all": {}}, "aggs": {"popular_terms": {"terms": {"field": "annotations.entities.name"}}}})
-    # return json.dumps(result, indent=4, sort_keys=True)
-    return result['aggregations']['popular_terms']['buckets']
+def get_top_trends(index=TR_INDEX):
+    db = ESClient(index)
+    popular_keywords = db.aggregate()
+    print json.dumps(popular_keywords, indent=4, sort_keys=True)
+    # most_popular_keyword = popular_keywords[0]['key']
+    # print 'Do you want to learn more about %s or %s news?' % (most_popular_keyword, popular_keywords[1]['key'])
+
+    # entities
+    popular_entities = [entity['key'].split(',')[0] for entity in popular_keywords['entities']['buckets']]
+    print 'News about %s and %s are trending right now!' % (popular_entities[0], popular_entities[1])
+    # tags
+    popular_tags = [tag['key'].replace('_', ' & ') for tag in popular_keywords['tags']['buckets']]
+    print 'Are you interested in %s or %s news?' % (popular_tags[0], popular_tags[1])
+
+
+def test_explore_trend(keyword='United States', index=TR_INDEX):
+    db = ESClient(index)
+    popular_keywords = db.explore_trend(keyword)
+    print json.dumps(popular_keywords[1:], indent=4, sort_keys=True)
+    key1 = popular_keywords[1]['key']
+    # print '%s and %s in %s' % (key1, popular_keywords[2]['key'], keyword)
+    # db.find_sample_article_by_keyword(key1)
+
+    # print json.dumps(subtrends, indent=4, sort_keys=True)
+
+
+# def explore_top_trends(index=TR_INDEX):
+#     db = ESClient(index)
+#     popular_keywords = db.aggregate()
+#     trends = (popular_keywords[0]['key'], popular_keywords[1]['key'])
 
 
 if __name__ == '__main__':
     # load_articles_in_ES()
-    check_n_docs()
-    popular_keywords = aggregate()
-    print popular_keywords[0]
+    # check_n_docs()
+    get_top_trends()
+    # test_explore_trend()
     # show_one()
